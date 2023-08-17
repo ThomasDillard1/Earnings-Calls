3930e7a3517875a3c7ed50cc567c355b2d306ace
Thomas <thomasdillard1@gmail.com>
2023-08-17 00:15:40 -0400

    creating search with manual list of companies

--- app/app.py
+++ app/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 from decouple import config
+import requests
 
 #Get the api key from the .env file
 API_KEY = config('API_KEY')
-
+API_BASE_URL = 'https://www.alphavantage.co'
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    companies = ['AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'GOOG', 'TSLA', 'BRK.B', 'UNH', 'JNJ', 'JPM', 'XOM', 'LLY', 'V', 'PG', 'AVGO', 'HD', 'MA', 'CVX', 'MRK', 'ABBV', 'PEP', 'COST', 'ADBE', 'KO', 'WMT', 'CSCO', 'MCD', 'BAC', 'TMO', 'CRM', 'PFE']
+    return render_template('index.html', companies=companies)
+    # if request.method == 'POST':
+    #    company = request.form['company_']
 
-# main driver function
+
 if __name__ == '__main__':
     app.run(debug=True)
